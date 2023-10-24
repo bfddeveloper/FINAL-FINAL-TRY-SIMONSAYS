cde87679c2216aeffd8bdd250c7c335f8153f53c
bfddeveloper <bdicksondev@gmail.com>
2023-10-24 13:37:00 -0500

    made squares and flashes

---
+++ FINAL FINAL TRY SIMONSAYS/ColorDisplay.swift
@@ -0,0 +1,25 @@
+//
+//  ColorDisplay.swift
+//  FINAL FINAL TRY SIMONSAYS
+//
+//  Created by Brody Dickson on 10/24/23.
+//
+
+//import SwiftUI
+//
+//struct ColorDisplay: View {
+//    let color: Color
+//    var body: some View {
+//        RoundedRectangle(cornerRadius: 25.0)
+//            .fill(color)
+//            .frame(width: 100, height: 100, alignment: .center)
+//            .padding()
+//    }
+//}
+//
+//
+//struct ColorDisplay_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ColorDisplay(color: Color)
+//    }
+//}
--- FINAL FINAL TRY SIMONSAYS/ContentView.swift
+++ FINAL FINAL TRY SIMONSAYS/ContentView.swift
@@ -8,16 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var flash = [false, false, false, false]
+    @State private var colorDisplay = [ColorDisplay(color: .green), ColorDisplay(color: .red), ColorDisplay(color: .yellow), ColorDisplay(color: .blue)]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack{
+                colorDisplay[0]
+                    .opacity(flash[0] ? 1 : 0.4)
+                colorDisplay[1]
+                    .opacity(flash[0] ? 1 : 0.4)
+            }
+            HStack{
+                colorDisplay[2]
+                    .opacity(flash[0] ? 1 : 0.4)
+                colorDisplay[3]
+                    .opacity(flash[0] ? 1 : 0.4)
+            }
+            
         }
         .padding()
     }
 }
+
+struct ColorDisplay: View {
+    let color: Color
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25.0)
+            .fill(color)
+            .frame(width: 100, height: 100, alignment: .center)
+            .padding()
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
